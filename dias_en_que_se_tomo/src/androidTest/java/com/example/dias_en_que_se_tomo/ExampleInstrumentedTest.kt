44e4742f9fff42d8c90b0435cd8bbd1289e789da
breadwbutter <al07003220@tecmilenio.mx>
2025-09-18 18:13:40 -0600

    Confirmacion

---
+++ dias_en_que_se_tomo/src/androidTest/java/com/example/dias_en_que_se_tomo/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.dias_en_que_se_tomo
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.dias_en_que_se_tomo.test", appContext.packageName)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ClassWork1_SA5_DM"
+include(":app")
+include(":mililitros_Tomados")
+include(":dias_en_que_se_tomo")
+include(":ranking_de_licores")

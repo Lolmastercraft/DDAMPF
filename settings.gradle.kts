475ceb4a49ae4c459abab29bb959ba368b66882b
Lolmastercraft <multifuncion77@gmail.com>
2025-09-18 18:07:07 -0600

    Primer Comit

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Proyecto Final"
+include(":app")
+include(":mililitros_tomados")
+include(":dias_en_que_se_tomo")
+include(":ranking_de_licores")
